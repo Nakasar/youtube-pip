1c126f5aae41e6e228209388bcf3222eabcc3555
THIZY Kevin <Nakasar@users.noreply.github.com>
2018-04-13 22:54:06 +0200

    1.0.0

---
+++ app.js
@@ -0,0 +1,61 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain;
+
+const path = require('path');
+const url = require('url');
+const pug = require('pug');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 370,
+    frame: false,
+    titleBarStyle: 'customButtonsOnHover',
+    transparent: true,
+    alwaysOnTop: true
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== darwin) {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipc.on('close', () => {
+  app.quit();
+});
+
+ipc.on('load-url', (event, arg) => {
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+});
